7a999eb5fb32c16fae5fc22640f95be347e52386
Abdul-Fatah <abdelfatah.ramadan42@gmail.com>
2020-04-19 10:45:30 +0200

    .

--- chat/models.py
+++ chat/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+
 # Create your models here.
 User = get_user_model()
+
+
 class Message(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_messages')
     content = models.TextField()
