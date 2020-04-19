651c1b05ba17c3e015a73f4e48dd56c8b926c28a
Abdul-Fatah <abdelfatah.ramadan42@gmail.com>
2020-04-19 13:23:41 +0200

    Update asgi.py

--- justchat/asgi.py
+++ justchat/asgi.py
@@ -8,7 +8,7 @@ import django
 from channels.layers import get_channel_layer
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "justchat.settings")
 django.setup()
 application = get_default_application()
 channel_layer = get_channel_layer()
